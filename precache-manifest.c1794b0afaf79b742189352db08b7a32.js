63a964eb51d78b181875992288fd8586214dcedd
Albert Miftar <a.miftar@aa.mk>
2020-07-02 13:13:03 +0200

    Updates

---
+++ precache-manifest.c1794b0afaf79b742189352db08b7a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97b9ffa31d6f7cbbb764284846acb57",
+    "url": "/pricelist/index.html"
+  },
+  {
+    "revision": "8e9f214d4b4143cbe5d6",
+    "url": "/pricelist/static/css/main.0b5a6a8a.chunk.css"
+  },
+  {
+    "revision": "3ee942747abd7f35e44b",
+    "url": "/pricelist/static/js/2.279cadfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pricelist/static/js/2.279cadfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9f214d4b4143cbe5d6",
+    "url": "/pricelist/static/js/main.ab26ee14.chunk.js"
+  },
+  {
+    "revision": "f838db5d0506ad38098d",
+    "url": "/pricelist/static/js/runtime-main.bd971746.js"
+  }
+]);
